b3ce8c9336b0d5a41ddd00f6a2f1eb1b81c9bf85
swcodingschool.com <swcodingschool@gmail.com>
2020-12-29 00:05:49 +0900

    2020-12-29 00:05 basic input and output

---
+++ Lesson001/src/com/swcodingschool/hello/ExSequentialFlow.java
@@ -0,0 +1,10 @@
+package com.swcodingschool.hello;
+
+public class ExSequentialFlow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lesson001/src/com/swcodingschool/hello/InputAndOutput.java
@@ -0,0 +1,19 @@
+package com.swcodingschool.hello;
+/*
+ * Java Basic Input and Output
+ * Java output : System.out.println(), ..print(), ..printf()
+ * Difference between println(), print() and printf()
+   print() - It prints string inside the quotes.
+   println() - It prints string inside the quotes similar like print() method. Then the cursor moves to the beginning of the next line.
+   printf() - It provides string formatting
+   
+   refer to  https://www.programiz.com/java-programming/basic-input-output
+ */
+public class InputAndOutput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
