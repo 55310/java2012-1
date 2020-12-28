64de0f0e77b19a8ef632d7fb4b9edebfd6e71608
swcodingschool <swcodingschool@gmail.com>
2020-12-28 12:27:29 +0900

    2020-12-28 first commit

---
+++ Lesson001/src/com/swcodingschool/hello/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.swcodingschool.hello;
+/*
+ * First Java Program, HelloWorld
+ * JDK, eclipse 설치 및 configuration 후 첫 프로그램 동작 확인
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java.");
+		System.out.println("By swcodingschool.com");
+
+	}
+
+}
