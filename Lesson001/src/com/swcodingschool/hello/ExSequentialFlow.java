bc28db2c5bc0c5daf8ba33d098f1164d11117e1c
swcodingschool.com <swcodingschool@gmail.com>
2020-12-29 00:07:42 +0900

    2020-12-29 00:07 add sequential flow examples

--- Lesson001/src/com/swcodingschool/hello/ExSequentialFlow.java
+++ Lesson001/src/com/swcodingschool/hello/ExSequentialFlow.java
@@ -1,5 +1,9 @@
 package com.swcodingschool.hello;
-
+/*
+ * Convert Fahrenheit to Celsius
+ * 
+ * Tc = 9*Tf/5 + 32
+ */
 public class ExSequentialFlow {
 
 	public static void main(String[] args) {
