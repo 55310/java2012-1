c39d120df5547eab3c6ac7d5f8e72d0586a45ba1
swcodingschool.com <swcodingschool@gmail.com>
2020-12-30 10:23:09 +0900

    2020-12-30 10:23

---
+++ Lesson001/src/com/swcodingschool/hello/Quiz1ConditionalStmt.java
@@ -0,0 +1,19 @@
+package com.swcodingschool.hello;
+/*
+ * 조건문 연습문제
+ * 성적을 입력받아 90이상은 A, 
+ *                 80이상은 B,
+ *                 70이상은 C,
+ *                 기타는 F 
+ * 로 등급을 부여하는 프로그램을
+ * 1. if~ elseif~ 구문을 이용하여 작성하고
+ * 2. switch case 구문을 이용하여 작성하라.  
+ */
+public class Quiz1ConditionalStmt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lesson001/src/com/swcodingschool/hello/Quiz2ForLoopStmt.java
@@ -0,0 +1,14 @@
+package com.swcodingschool.hello;
+/*
+ * 다중 for loop 연습문제
+ * 구구단을 출력하는 프로그램을 다중 for loop을
+ * 이용하여 작성하시오.
+ */
+public class Quiz2ForLoopStmt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
